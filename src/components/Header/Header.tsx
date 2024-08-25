800d1b3414de371f81237020b478e1cb28dfe2b2
mhelenacipriano <mariahelenatw@gmail.com>
2024-08-25 19:49:49 -0300

    feat: header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../../styles/Header.module.css";
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <p className={styles.title}>Music.Ai</p>
+    </div>
+  );
+};
