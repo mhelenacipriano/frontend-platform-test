be26682b55e865c25e7b91c8e6978ae12e58da3d
mhelenacipriano <mariahelenatw@gmail.com>
2024-08-26 08:51:26 -0300

    feat: changing title

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,7 +76,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Music.ai</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
